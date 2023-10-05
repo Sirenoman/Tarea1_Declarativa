dfb5a1f95f3135b290560c787714b56305398dd6
Sirenoman <00003120@uca.edu.sv>
2023-10-05 11:51:49 -0600

    Add coordinates

--- tareaProlog.pl
+++ tareaProlog.pl
@@ -26,4 +26,33 @@ lugar(plaza_las_ramblas).
 %Calles de lugares
 %
 
+% COORDENADAS
+% Sintaxis de coordenadas, agregadas como predicados:
+% -> coordenada(lugar, coordenadaX, coordenadaY).
+%
+% Centro de estudio
+coordenada(colegio_santa_cecilia, 546, 296).
+coordenada(itca_fepade, 714, 407).
+coordenada(fesa, 563, 207).
+
+% Centro de Salud
+coordenada(hospital_san_rafael, 740, 435).
+
+% Centro de Gasolinera
+coordenada(gasolineria_uno, 300, 418).
+
+% Centro de parques recreativos
+coordenadas(parque_daniel_hernandez, 529, 399).
+coordenadas(parque_san_martin, 583, 402).
+coordenadas(parque_cafetalon, 640, 326).
+coordenadas(parque_la_familia, 647, 183).
+coordenadas(estadio_nacional_las_delicias, 273, 416).
+coordenadas(parque_ecologico_san_jose, 456, 61).
+
+% Centros de centro comercial
+coordenadas(plaza_merliot, 791, 269).
+coordenadas(plaza_la_skina, 306, 287).
+coordenadas(plaza_las_ramblas,  975, 460).
+
+%Calle de lugares
 

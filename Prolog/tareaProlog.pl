4a9372557efef1d3527b96295795cd35c4fafe89
Sirenoman <00003120@uca.edu.sv>
2023-10-05 11:53:35 -0600

    Add coordinates

---
+++ Prolog/tareaProlog.pl
@@ -0,0 +1,58 @@
+%Base de conocimiento para la tarea de Declarativa
+%Centros de estudio
+lugar(colegio_santa_cecilia).
+lugar(itca_fepade).
+lugar(fesa).
+
+%Centros de salud
+lugar(hospital_san_rafael).
+
+%Centros de gasolineria
+lugar(gasolineria_uno).
+
+%Centros de parques recreativos
+lugar(parque_daniel_hernandez).
+lugar(parque_san_martin).
+lugar(parque_cafetalon).
+lugar(parque_la_familia).
+lugar(estadio_nacional_las_delicias).
+lugar(parque_ecologico_san_jose).
+
+%Centros de centro comercial
+lugar(plaza_merliot).
+lugar(plaza_la_skina).
+lugar(plaza_las_ramblas).
+
+%Calles de lugares
+%
+
+% COORDENADAS
+% Sintaxis de coordenadas, agregadas como predicados:
+% -> coordenada(lugar, coordenadaX, coordenadaY).
+%
+% Centro de estudio
+coordenada(colegio_santa_cecilia, 546, 296).
+coordenada(itca_fepade, 714, 407).
+coordenada(fesa, 563, 207).
+
+% Centro de Salud
+coordenada(hospital_san_rafael, 740, 435).
+
+% Centro de Gasolinera
+coordenada(gasolineria_uno, 300, 418).
+
+% Centro de parques recreativos
+coordenadas(parque_daniel_hernandez, 529, 399).
+coordenadas(parque_san_martin, 583, 402).
+coordenadas(parque_cafetalon, 640, 326).
+coordenadas(parque_la_familia, 647, 183).
+coordenadas(estadio_nacional_las_delicias, 273, 416).
+coordenadas(parque_ecologico_san_jose, 456, 61).
+
+% Centros de centro comercial
+coordenadas(plaza_merliot, 791, 269).
+coordenadas(plaza_la_skina, 306, 287).
+coordenadas(plaza_las_ramblas,  975, 460).
+
+%Calle de lugares
+
